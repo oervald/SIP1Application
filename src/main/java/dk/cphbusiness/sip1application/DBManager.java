3f8f74e78d4981a9c01ea65c65eb52b3917b5bac
Oervald <neo24dk@gmail.com>
2015-02-23 10:53:22 +0100

    Started on making the SQL server setUp

---
+++ src/main/java/dk/cphbusiness/sip1application/DBManager.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package dk.cphbusiness.sip1application;
+
+/**
+ *
+ * @author Henrik
+ */
+public class DBManager {
+    
+    
+    
+}
