83c1977a577a15eae203b6197a6075e60ed87fce
Oervald <neo24dk@gmail.com>
2015-03-06 13:41:05 +0100

    Casper/Henrik/Nikos/David Test created and more functionlality

--- src/main/java/EnityClass/EmManager.java
+++ src/main/java/EnityClass/EmManager.java
@@ -8,9 +8,14 @@ package EnityClass;
 
 import Interfaces.DBFacade;
 import java.util.ArrayList;
+import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import javax.persistence.Query;
+import org.eclipse.persistence.internal.jpa.EntityManagerImpl;
+import utilities.CloseableManager;
 
 /**
  *
@@ -18,27 +23,87 @@ import javax.persistence.Persistence;
  */
 public class EmManager implements DBFacade {
 
-     EntityManagerFactory emf = Persistence.createEntityManagerFactory("dk.cphbusiness.oervald_SIP1Application_jar_1.0-SNAPSHOTPU");
-    EntityManager em = emf.createEntityManager();
+     EntityManagerFactory emf = Persistence.createEntityManagerFactory("dk.cphbusiness.oervald_SIP1Application");
+    
+    
     
     @Override
-    public ArrayList getAllProposals() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public List getAllProposals() {
+         EntityManager em = emf.createEntityManager();
+        List<Proposal> proposals = null;
+        Query query = em.createNamedQuery("Proposal.getAllProposals");
+        EntityTransaction transaction = em.getTransaction();
+        transaction.begin();
+        try{
+        
+        proposals = (List<Proposal>) query.getResultList();
+        
+        
+        }catch(Exception e){
+            System.err.println(e.getMessage());
+            transaction.rollback();
+        }
+        em.close();
+        return proposals;
     }
 
     @Override
     public void addProposal(Proposal proposal) {
-        em.getTransaction();
-        em.persist(proposal);
+         EntityManager em = emf.createEntityManager();
+        EntityTransaction transaction = em.getTransaction();
+        transaction.begin();
+         try {
+            em.persist(proposal);
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+            transaction.rollback();
+        }
+         System.out.println(proposal);
+        transaction.commit();
         em.close();
+     
     }
 
     @Override
     public Proposal getOneProposal(String title) {
-        em.getTransaction();
-       Proposal proposal = em.find(Proposal.class, title);
-       em.close();
-       return proposal;
+         EntityManager em = emf.createEntityManager();
+        List<Proposal> proposal = null;
+       
+        EntityTransaction transaction = em.getTransaction();
+        transaction.begin();
+        
+        try{
+         Query query = em.createNamedQuery("Proposal.findOneProposal");
+        query.setParameter("title", title);
+         proposal = (List<Proposal>) query.getResultList();
+            System.out.println(" getOneProposal in try loop :" + proposal.toString());
+        }catch(Exception e){
+        System.err.println(e.getMessage());
+        }
+        
+        em.close();
+        System.out.println(" before return:  " + proposal.get(0));
+       return proposal.get(0);
     }
+
+     @Override
+    public void addPerson(Person person) {
+         EntityManager em = emf.createEntityManager();
+        
+        em.getTransaction();
+        
+        em.persist(person);
+    }
+
+    @Override
+    public void addToPool(Proposal proposal, Pool pool) {
+         EntityManager em = emf.createEntityManager();
+        String title = proposal.getTitle();
+        Proposal p = getOneProposal(title);
+        em.getTransaction();
+        int id =  pool.getId();
+        
+    }
+
     
 }
